45671b3fde23ece71752553f94aa3001eaf05b6c
Lars Anker <75078152+larsanker50@users.noreply.github.com>
2021-03-05 13:11:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ let card6 = new Deck(7, "heart", 7);
 let card7 = new Deck(8, "heart", 8);
 let card8 = new Deck(9, "heart", 9);
 let card9 = new Deck(10, "heart", 10);
-let card10 = new Deck(10, "heart", "jack");
-let card11 = new Deck(10, "heart", "queen");
-let card12 = new Deck(10, "heart", "king");
-let card13 = new Deck(11, "heart", "ace");
+let card10 = new Deck(11, "heart", "jack");
+let card11 = new Deck(12, "heart", "queen");
+let card12 = new Deck(13, "heart", "king");
+let card13 = new Deck(14, "heart", "ace");
 let card14 = new Deck(2, "club", 2);
 let card15 = new Deck(3, "club", 3);
 let card16 = new Deck(4, "club", 4);
@@ -46,10 +46,10 @@ let card19 = new Deck(7, "club", 7);
 let card20 = new Deck(8, "club", 8);
 let card21 = new Deck(9, "club", 9);
 let card22 = new Deck(10, "club", 10);
-let card23 = new Deck(10, "club", "jack");
-let card24 = new Deck(10, "club", "queen");
-let card25 = new Deck(10, "club", "king");
-let card26 = new Deck(11, "club", "ace");
+let card23 = new Deck(11, "club", "jack");
+let card24 = new Deck(12, "club", "queen");
+let card25 = new Deck(13, "club", "king");
+let card26 = new Deck(14, "club", "ace");
 let card27 = new Deck(2, "diamond", 2);
 let card28 = new Deck(3, "diamond", 3);
 let card29 = new Deck(4, "diamond", 4);
@@ -59,10 +59,10 @@ let card32 = new Deck(7, "diamond", 7);
 let card33 = new Deck(8, "diamond", 8);
 let card34 = new Deck(9, "diamond", 9);
 let card35 = new Deck(10, "diamond", 10);
-let card36 = new Deck(10, "diamond", "jack");
-let card37 = new Deck(10, "diamond", "queen");
-let card38 = new Deck(10, "diamond", "king");
-let card39 = new Deck(11, "diamond", "ace");
+let card36 = new Deck(11, "diamond", "jack");
+let card37 = new Deck(12, "diamond", "queen");
+let card38 = new Deck(13, "diamond", "king");
+let card39 = new Deck(14, "diamond", "ace");
 let card40 = new Deck(2, "spade", 2);
 let card41 = new Deck(3, "spade", 3);
 let card42 = new Deck(4, "spade", 4);
@@ -72,12 +72,12 @@ let card45 = new Deck(7, "spade", 7);
 let card46 = new Deck(8, "spade", 8);
 let card47 = new Deck(9, "spade", 9);
 let card48 = new Deck(10, "spade", 10);
-let card49 = new Deck(10, "spade", "jack");
-let card50 = new Deck(10, "spade", "queen");
-let card51 = new Deck(10, "spade", "king");
-let card52 = new Deck(11, "spade", "ace");
-let card53 = new Deck(12, "joker", "joker");
-let card54 = new Deck(12, "joker", "joker");
+let card49 = new Deck(11, "spade", "jack");
+let card50 = new Deck(12, "spade", "queen");
+let card51 = new Deck(13, "spade", "king");
+let card52 = new Deck(14, "spade", "ace");
+let card53 = new Deck(15, "joker", "joker");
+let card54 = new Deck(15, "joker", "joker");
 
 
 function randomPlayerCard() {
@@ -174,15 +174,21 @@ document.getElementById("beurtKnop").addEventListener("click", function () {
         dealerCard[0].classList.remove("brown");
         scorePlayer = playerCardsArray[0].value;
         scoreDealer = dealerCardsArray[0].value;
+        if (scorePlayer == 2 && scoreDealer == 15) {
+            scoreDealer == 1
+        };
+        if (scoreDealer == 2 && scorePlayer == 15) {
+            scorePlayer == 1
+        };
         if (scorePlayer == 3) {
-            let random = Math.floor((Math.random() * 1) + 1);
+            let random = Math.floor((Math.random() * 5) + 1);
             if (random == 1) {
                 scorePlayer = 100;
                 drieBlieHTML.innerHTML = "Speler zegt: DRIE BLIE!!!!!";
             };
         };
         if (scoreDealer == 3) {
-            let random = Math.floor((Math.random() * 1) + 1);
+            let random = Math.floor((Math.random() * 5) + 1);
             if (random == 1) {
                 scoreDealer = 100;
                 drieBlieHTML.innerHTML = "Computer zegt: DRIE BLIE!!!!!";
